0a5ca7491f94f0f48ef97a3b1467e43d32206b88
kmg0512 <mg_kwon@naver.com>
2019-05-30 02:40:40 +0900

    Save

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.mousePosition.x;
+        float z = Input.mousePosition.z;
+
+        if (x > 0)      x = 0.0f;
+        if (x < -2.5)   x = -2.5f;
+        if (z > 1)      z = 1.0f;
+        if (z < -1)     z = -1.0f;
+
+        this.transform.position = new Vector3(x, this.transform.position.y, z);
+    }
+}
