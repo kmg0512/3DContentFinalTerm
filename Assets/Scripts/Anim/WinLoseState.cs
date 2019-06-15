08c48d1035e1a966ec01c599a7b6bb1ddd8e7e49
kmg0512 <mg_kwon@naver.com>
2019-06-15 19:20:44 +0900

    Unity-chan Win-Lose Animation

---
+++ Assets/Scripts/Anim/WinLoseState.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinLoseState : MonoBehaviour
+{
+    Animator animator;
+    public int player;
+    private bool flag;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        flag = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TopGL.gameStatus == 5 && flag)
+        {
+            if(player == TopGL.beforeGoalWho)
+            {
+                animator.SetTrigger("Win");
+            }
+            else
+            {
+                animator.SetTrigger("Lose");
+            }
+            flag = false;
+        }
+    }
+}
