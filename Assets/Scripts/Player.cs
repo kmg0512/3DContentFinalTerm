aa51cef445c9f826de8435cdbf1aa77c36842121
kmg0512 <mg_kwon@naver.com>
2019-06-15 15:17:37 +0900

    Pad input

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,21 +4,31 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float strength;
     public Rigidbody rb;
     public float speed;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        speed = TopGL.initialSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Horizontal"))
-        {
-            this.transform.Translate(speed * Time.deltaTime, 0, 0);
-        }
+        Vector3 moveVector = new Vector3(0.0f, 0.0f, 0.0f);
+
+        // update moveVector
+        if (Input.GetKey(KeyCode.W)) moveVector.z = 1.0f;
+        if (Input.GetKey(KeyCode.A)) moveVector.x += -1.0f;
+        if (Input.GetKey(KeyCode.S)) moveVector.z = -1.0f;
+        if (Input.GetKey(KeyCode.D)) moveVector.x += 1.0f;
+        moveVector.x += Input.GetAxis("Pad1X");
+        moveVector.z -= Input.GetAxis("Pad1Y");
+
+
+        // FORCE WILL BE WITH YOU
+        rb.AddForce(speed * moveVector, ForceMode.Impulse);
     }
 }
